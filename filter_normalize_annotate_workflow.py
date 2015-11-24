41c5a20804f11b664bc31a4493f1c59c1e92e0b3
Dan Gaston <daniel.gaston@gmail.com>
2015-11-24 13:49:54 -0400

    tweaking workflows

--- filter_normalize_annotate_workflow.py
+++ filter_normalize_annotate_workflow.py
@@ -11,11 +11,10 @@ import multiprocessing
 from toil.job import Job
 
 # Package methods
+from ngsflow import gatk
 from ngsflow import annotation
-from ngsflow import read_sample_sheet
 from ngsflow.utils import configuration
 from ngsflow.utils import utilities
-from ngsflow.variation import variation
 
 
 if __name__ == "__main__":
@@ -39,3 +38,22 @@ if __name__ == "__main__":
     for sample in samples:
         on_target_job = Job.wrapJobFn(utilities.bcftools_filter_variants_regions, config, sample,
                                       samples[sample]['vcf'], cores=num_cores, memory="1G")
+        gatk_annotate_job = Job.wrapJobFn(gatk.annotate_vcf, config, sample, on_target_job.rv(), samples[sample]['bam'],
+                                          cores=num_cores, memory="4G")
+        gatk_filter_job = Job.wrapJobFn(gatk.filter_variants, config, sample, gatk_annotate_job.rv(),
+                                        cores=1, memory="2G")
+        normalization_job = Job.wrapJobFn(utilities.vt_normalization, config, sample, gatk_filter_job.rv(),
+                                          cores=1, memory="2G")
+        snpeff_job = Job.wrapJobFn(annotation.snpeff, config, sample, normalization_job.rv(),
+                                   cores=num_cores, memory="4G")
+        gemini_job = Job.wrapJobFn(annotation.gemini, config, sample, snpeff_job.rv(),
+                                   cores=num_cores, memory="4G")
+
+        root_job.addChild(on_target_job)
+        on_target_job.addChild(gatk_annotate_job)
+        gatk_annotate_job.addChild(gatk_filter_job)
+        gatk_filter_job.addChild(normalization_job)
+        normalization_job.addChild(snpeff_job)
+        snpeff_job.addChild(gemini_job)
+
+    Job.Runner.startToil(root_job, args)
