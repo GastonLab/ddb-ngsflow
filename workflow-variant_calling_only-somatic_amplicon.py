fa5eab32fdf07e91210e2ad89961cc59f8f4b932
Dan Gaston <daniel.gaston@gmail.com>
2016-01-08 15:17:00 -0400

    Tweaked variant calling only algorithm

--- workflow-variant_calling_only-somatic_amplicon.py
+++ workflow-variant_calling_only-somatic_amplicon.py
@@ -8,12 +8,8 @@ import argparse
 from toil.job import Job
 
 # Package methods
-from ngsflow import gatk
-from ngsflow import annotation
-from ngsflow.align import bwa
 from ngsflow.utils import configuration
 from ngsflow.utils import utilities
-from ngsflow.variation import variation
 from ngsflow.variation import freebayes
 from ngsflow.variation import mutect
 from ngsflow.variation import platypus
@@ -42,34 +38,34 @@ if __name__ == "__main__":
 
     # Per sample variant calling jobs
     for sample in samples:
-        # freebayes_job = Job.wrapJobFn(freebayes.freebayes_single, config, sample, sample['bam'],
+        # freebayes_job = Job.wrapJobFn(freebayes.freebayes_single, config, sample, samples[sample]['bam'],
         #                               cores=1,
         #                               memory="{}G".format(config['freebayes']['max_mem']))
         #
-        # mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, sample['bam'],
+        # mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, samples[sample]['bam'],
         #                            cores=1,
         #                            memory="{}G".format(config['mutect']['max_mem']))
 
-        vardict_job = Job.wrapJobFn(vardict.vardict_single, config, sample, samples, sample['bam'],
+        vardict_job = Job.wrapJobFn(vardict.vardict_single, config, sample, samples, samples[sample]['bam'],
                                     cores=int(config['vardict']['num_cores']),
                                     memory="{}G".format(config['vardict']['max_mem']))
 
-        scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, sample['bam'],
+        scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, samples[sample]['bam'],
                                     cores=int(config['scalpel']['num_cores']),
                                     memory="{}G".format(config['scalpel']['max_mem']))
 
-        # indelminer_job = Job.wrapJobFn(indelminer.indelminer_single, config, sample, sample['bam'],
+        # indelminer_job = Job.wrapJobFn(indelminer.indelminer_single, config, sample, samples[sample]['bam'],
         #                                cores=1,
         #                                memory="{}G".format(config['indelminer']['max_mem']))
 
         # Platypus defined regions not created for all panels. Need to handle stranded pool libraries
-        # platypus_job = Job.wrapJobFn(platypus.platypus_single, config, sample, sample['bam'],
+        # platypus_job = Job.wrapJobFn(platypus.platypus_single, config, sample, samples[sample]['bam'],
         #                              cores=int(config['platypus']['num_cores']),
         #                              memory="{}G".format(config['platypus']['max_mem']))
 
         # Create workflow from created jobs
-        root_job.addChild(freebayes_job)
-        root_job.addChild(mutect_job)
+        # root_job.addChild(freebayes_job)
+        # root_job.addChild(mutect_job)
         root_job.addChild(vardict_job)
         root_job.addChild(scalpel_job)
         # root_job.addChild(indelminer_job)
