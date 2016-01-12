2d6b8857f225457784bf1134efb1ce0e753dc7bf
Dan Gaston <daniel.gaston@gmail.com>
2016-01-12 16:15:52 -0400

    Renaming script

--- workflow-variant_calling_only-somatic_amplicon.py
+++ workflow-variant_calling_only-somatic_amplicon.py
@@ -42,17 +42,17 @@ if __name__ == "__main__":
         #                               cores=1,
         #                               memory="{}G".format(config['freebayes']['max_mem']))
         #
-        # mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, samples[sample]['bam'],
-        #                            cores=1,
-        #                            memory="{}G".format(config['mutect']['max_mem']))
+        mutect_job = Job.wrapJobFn(mutect.mutect_single, config, sample, samples[sample]['bam'],
+                                   cores=1,
+                                   memory="{}G".format(config['mutect']['max_mem']))
 
         vardict_job = Job.wrapJobFn(vardict.vardict_single, config, sample, samples, samples[sample]['bam'],
                                     cores=int(config['vardict']['num_cores']),
                                     memory="{}G".format(config['vardict']['max_mem']))
 
-        scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, samples[sample]['bam'],
-                                    cores=int(config['scalpel']['num_cores']),
-                                    memory="{}G".format(config['scalpel']['max_mem']))
+        # scalpel_job = Job.wrapJobFn(scalpel.scalpel_single, config, sample, samples, samples[sample]['bam'],
+        #                             cores=int(config['scalpel']['num_cores']),
+        #                             memory="{}G".format(config['scalpel']['max_mem']))
 
         # indelminer_job = Job.wrapJobFn(indelminer.indelminer_single, config, sample, samples[sample]['bam'],
         #                                cores=1,
@@ -65,9 +65,9 @@ if __name__ == "__main__":
 
         # Create workflow from created jobs
         # root_job.addChild(freebayes_job)
-        # root_job.addChild(mutect_job)
+        root_job.addChild(mutect_job)
         root_job.addChild(vardict_job)
-        root_job.addChild(scalpel_job)
+        # root_job.addChild(scalpel_job)
         # root_job.addChild(indelminer_job)
         # root_job.addChild(platypus_job)
 
