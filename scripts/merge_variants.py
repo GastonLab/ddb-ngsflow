052e11926c667f4352d7a73b1fbb48778bada9dd
Dan Gaston <daniel.gaston@gmail.com>
2016-01-14 15:31:05 -0400

    Testing CyVCF-based merging script

--- scripts/merge_variants.py
+++ scripts/merge_variants.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
-import argparse
-from cyvcf2 import VCF
+import sys
+import cyvcf
+from cyvcf import utils
 
+vcf_readers = list()
+for vcffile in sys.argv[1:]:
+    vcf_reader = cyvcf.Reader(open(vcffile, 'rb'))
+    vcf_readers.append(vcf_reader)
+
+for caller_records in utils.walk_together(*vcf_readers):
+    num_callers = 0
+    for record in caller_records:
+        if record is not None:
+            num_callers += 1
+    if num_callers > 1:
+        for record in caller_records:
+            sys.stdout.write("*")
+            print record
+        sys.stdout.write("\n")
