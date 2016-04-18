b8dfc0c146eb16ddd968b0e5f6a662f38d1acca3
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-04-18 08:56:26 -0300

    Temp fix

--- ddb_ngsflow/variation/mutect.py
+++ ddb_ngsflow/variation/mutect.py
@@ -77,7 +77,7 @@ def mutect_single(job, config, name, samples, input_bam):
                       "{}".format(config['vcftools_subset']['bin']),
                       "-e",
                       "-c",
-                      "{}".format(samples[name]['library_name']),
+                      "{}".format(name),
                       ">",
                       "{}".format(mutect_vcf))
 
