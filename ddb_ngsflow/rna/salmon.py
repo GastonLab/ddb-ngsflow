1655e2d2d07f17969f5fa57e43b8e62d7b237e84
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-05-18 10:23:59 -0300

    Adding RNA-Seq wrappers

---
+++ ddb_ngsflow/rna/salmon.py
@@ -0,0 +1,44 @@
+"""
+.. module:: salmon
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the salmon RNA-Seq program
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+import pipeline
+
+
+def salmon_paired(job, config, name, samples):
+    """Annotate the specified VCF using snpEff
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output vcf file name.
+    """
+
+    output_dir = "{}.salmon.output".format(name)
+    logfile = "{}.salmon.log".format(name)
+
+    command = ("{} quant".format(config['salmon']['bin']),
+               "-i {}".format(config['salmon']['index']),
+               "-l {}".format(samples[name]['library_type']),
+               "-p {}".format(config['salmon']['num_cores']),
+               "--useVBOpt",
+               "--numBootstraps {}".format(config['salmon']['num_bootstraps']),
+               "--biasCorrect",
+               "--useFSPD",
+               "-r {}".format(samples[name]['fastq1']),
+               "-o {}".format(output_dir)
+               )
+
+    job.fileStore.logToMaster("Salmon Command: {}\n".format(command))
+    pipeline.run_and_log_command(command, logfile)
+
+    return output_dir
