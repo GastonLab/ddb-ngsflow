be0b5f871512f439cce364d3cf2f050cdd1f59f0
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-05-24 13:34:22 -0300

    Skeleton for STAR and cufflinks methods

---
+++ ddb_ngsflow/rna/cufflinks.py
@@ -0,0 +1,12 @@
+"""
+.. module:: cufflinks
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the cufflinks RNA-Seq programs
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+from ddb_ngsflow import pipeline
---
+++ ddb_ngsflow/rna/star.py
@@ -0,0 +1,12 @@
+"""
+.. module:: star
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the STAR RNA alignment program
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+from ddb_ngsflow import pipeline
