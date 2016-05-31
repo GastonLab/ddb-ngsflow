44be0097360eb648555a34924b349155b249f1f9
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-05-31 14:28:22 -0300

    Add EM using version of unpaired method

--- ddb_ngsflow/rna/salmon.py
+++ ddb_ngsflow/rna/salmon.py
@@ -75,3 +75,34 @@ def salmon_unpaired(job, config, name, samples):
     pipeline.run_and_log_command(command, logfile)
 
     return output_dir
+
+
+def salmonEM_unpaired(job, config, name, samples):
+    """Annotate the specified VCF using snpEff
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output vcf file name.
+    """
+
+    output_dir = "{}.salmon.output".format(name)
+    logfile = "{}.salmon.log".format(name)
+
+    command = ("{} quant".format(config['salmon']['bin']),
+               "-i {}".format(config['salmon']['index']),
+               "-l {}".format(samples[name]['library_type']),
+               "-p {}".format(config['salmon']['num_cores']),
+               "--numBootstraps {}".format(config['salmon']['num_bootstraps']),
+               "--biasCorrect",
+               "--useFSPD",
+               "-r {}".format(samples[name]['fastq1']),
+               "-o {}".format(output_dir)
+               )
+
+    job.fileStore.logToMaster("Salmon Command: {}\n".format(command))
+    pipeline.run_and_log_command(command, logfile)
+
+    return output_dir
