cb8b1a48864e67d117f10220498b4973664434dc
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-01 15:23:32 -0300

    Adding bowtie2 wrapper methods

---
+++ ddb_ngsflow/rna/bowtie.py
@@ -0,0 +1,67 @@
+"""
+.. module:: cufflinks
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the bowtie alignment program
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+from ddb_ngsflow import pipeline
+
+
+def bowtie_unpaired(job, config, name, samples):
+    """Align RNA-Seq data to a reference using Bowtie2
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output vcf file name.
+    """
+
+    output = "{}.bowtie.sam".format(name)
+    logfile = "{}.bowtie.log".format(name)
+
+    command = ("{}".format(config['bowtie']['bin']),
+               "-x {}".format(config['bowtie']['index']),
+               "-p {}".format(config['bowtie']['num_cores']),
+               "-U {}".format(samples[name]['fastq1']),
+               "-S {}".format(output)
+               )
+
+    job.fileStore.logToMaster("Bowtie Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return output
+
+
+def bowtie_paired(job, config, name, samples):
+    """Align RNA-Seq data to a reference using Bowtie2
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output vcf file name.
+    """
+
+    output = "{}.bowtie.sam".format(name)
+    logfile = "{}.bowtie.log".format(name)
+
+    command = ("{}".format(config['bowtie']['bin']),
+               "-x {}".format(config['bowtie']['index']),
+               "-p {}".format(config['bowtie']['num_cores']),
+               "-1 {}".format(samples[name]['fastq1']),
+               "-2 {}".format(samples[name]['fastq1']),
+               "-S {}".format(output)
+               )
+
+    job.fileStore.logToMaster("Bowtie Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return output
