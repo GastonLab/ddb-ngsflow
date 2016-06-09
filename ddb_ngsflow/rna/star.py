241994771278fd862c4e8f10209501366cfb4280
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-09 13:56:31 -0300

    Updating STAR alignment with SAM to BAM conversion

--- ddb_ngsflow/rna/star.py
+++ ddb_ngsflow/rna/star.py
@@ -41,7 +41,11 @@ def star_paired(job, config, name, samples, flags):
     """
 
     output = "{}.star.output".format(name)
+    output_sam = "{}Aligned.out.sam".format()
     logfile = "{}.star.log".format(name)
+    sort_logfile = "{}.sortconvert.log".format(name)
+    temp = "{}.bwa.sort.temp".format(name)
+    output_bam = "{}.star.bam".format(name)
 
     command = ["{}".format(config['star']['bin']),
                "--genomeDir {}".format(config['star']['index']),
@@ -53,13 +57,37 @@ def star_paired(job, config, name, samples, flags):
 
     command = add_additional_options(command, config, flags)
 
+    view_cmd = ["{}".format(config['samtools']['bin']),
+                "view",
+                "-T {}".format(config['reference']),
+                "-u",
+                "{}".format(output_sam)
+                ]
+
+    sort_cmd = ["{}".format(config['samtools']['bin']),
+                "sort",
+                "-@",
+                "{}".format(config['star']['num_cores']),
+                "-O",
+                "bam",
+                "-o",
+                "{}".format(output_bam),
+                "-T",
+                "{}".format(temp),
+                "-"
+                ]
+
+    sort_command = "{} | {}".format(" ".join(view_cmd), " ".join(sort_cmd))
+
     job.fileStore.logToMaster("STAR Command: {}\n".format(command))
     pipeline.run_and_log_command(" ".join(command), logfile)
 
-    aligned_sam = "{}Aligned.out.sam".format(output)
-    samples[name]['bam'] = aligned_sam
+    job.fileStore.logToMaster("Sort and Convert Command: {}\n".format(sort_command))
+    pipeline.run_and_log_command(" ".join(sort_command), sort_logfile)
 
-    return aligned_sam
+    samples[name]['bam'] = output_bam
+
+    return output_bam
 
 
 def star_unpaired(job, config, name, samples, flags):
@@ -93,100 +121,3 @@ def star_unpaired(job, config, name, samples, flags):
     samples[name]['bam'] = aligned_sam
 
     return output
-
-
-# def star_paired_compressed_basic(job, config, name, samples):
-#     """Align RNA-Seq data to a reference using STAR
-#     :param config: The configuration dictionary.
-#     :type config: dict.
-#     :param name: sample name.
-#     :type name: str.
-#     :param samples: The samples info and config dictionary.
-#     :type samples: dict.
-#     :returns:  str -- The output vcf file name.
-#     """
-#
-#     output_dir = "{}.star.output".format(name)
-#     logfile = "{}.star.log".format(name)
-#
-#     command = ("{}".format(config['star']['bin']),
-#                "--genomeDir {}".format(config['star']['index']),
-#                "--runThreadN {}".format(config['star']['num_cores']),
-#                "--readFilesIn {} {}".format(samples[name]['fastq1'], samples[name]['fastq2']),
-#                "--readFilesCommand {}".format(config['compression']),
-#                "--outFileNamePrefix {}".format(output_dir)
-#                )
-#
-#     job.fileStore.logToMaster("STAR Command: {}\n".format(command))
-#     pipeline.run_and_log_command(" ".join(command), logfile)
-#
-#     return output_dir
-#
-#
-# def star_paired_uncompressed_encode(job, config, name, samples):
-#     """Align RNA-Seq data to a reference using STAR
-#     :param config: The configuration dictionary.
-#     :type config: dict.
-#     :param name: sample name.
-#     :type name: str.
-#     :param samples: The samples info and config dictionary.
-#     :type samples: dict.
-#     :returns:  str -- The output vcf file name.
-#     """
-#
-#     output_dir = "{}.star.output".format(name)
-#     logfile = "{}.star.log".format(name)
-#
-#     command = ("{}".format(config['star']['bin']),
-#                "--genomeDir {}".format(config['star']['index']),
-#                "--runThreadN {}".format(config['star']['num_cores']),
-#                "--readFilesIn {} {}".format(samples[name]['fastq1'], samples[name]['fastq2']),
-#                "--outFilterType BySJout",
-#                "--outFilterMultimapNmax 20",
-#                "--alignSJDBoverhangMin 1",
-#                "--outFilterMismatchNMax 999",
-#                "--alignIntronMin 20",
-#                "--alignIntronMax 1000000",
-#                "--alignMatesGapMax 1000000",
-#                "--outFileNamePrefix {}".format(output_dir)
-#                )
-#
-#     job.fileStore.logToMaster("STAR Command: {}\n".format(command))
-#     pipeline.run_and_log_command(" ".join(command), logfile)
-#
-#     return output_dir
-#
-#
-# def star_paired_compressed_encode(job, config, name, samples):
-#     """Align RNA-Seq data to a reference using STAR
-#     :param config: The configuration dictionary.
-#     :type config: dict.
-#     :param name: sample name.
-#     :type name: str.
-#     :param samples: The samples info and config dictionary.
-#     :type samples: dict.
-#     :returns:  str -- The output vcf file name.
-#     """
-#
-#     output_dir = "{}.star.output".format(name)
-#     logfile = "{}.star.log".format(name)
-#
-#     command = ("{}".format(config['star']['bin']),
-#                "--genomeDir {}".format(config['star']['index']),
-#                "--runThreadN {}".format(config['star']['num_cores']),
-#                "--readFilesIn {} {}".format(samples[name]['fastq1'], samples[name]['fastq2']),
-#                "--outFilterType BySJout",
-#                "--outFilterMultimapNmax 20",
-#                "--alignSJDBoverhangMin 1",
-#                "--outFilterMismatchNMax 999",
-#                "--alignIntronMin 20",
-#                "--alignIntronMax 1000000",
-#                "--alignMatesGapMax 1000000",
-#                "--outFileNamePrefix {}".format(output_dir),
-#                "--readFilesCommand {}".format(config['compression'])
-#                )
-#
-#     job.fileStore.logToMaster("STAR Command: {}\n".format(command))
-#     pipeline.run_and_log_command(" ".join(command), logfile)
-#
-#     return output_dir
