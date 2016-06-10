129363c86310891a43d8d546eede6f51970455f6
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-10 15:15:16 -0300

    Salmon alignment mode wrapper

--- ddb_ngsflow/rna/salmon.py
+++ ddb_ngsflow/rna/salmon.py
@@ -138,3 +138,34 @@ def salmonEM_unpaired(job, config, name, samples):
     pipeline.run_and_log_command(" ".join(command), logfile)
 
     return output_dir
+
+
+def salmonAlignEM(job, config, name, samples):
+    """Run Salmon Quasi-Mapping with single end data using the EM optimization algorithm
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output vcf file name.
+    """
+
+    output_dir = "{}.salmon_quant".format(name)
+    logfile = "{}.salmon.log".format(name)
+
+    command = ["{} quant".format(config['salmon']['bin']),
+               "-t {}".format(config['salmon']['transcripts']),
+               "-l {}".format(samples[name]['library_type']),
+               "-p {}".format(config['salmon']['num_cores']),
+               # "--numBootstraps {}".format(config['salmon']['num_bootstraps']),
+               "--biasCorrect",
+               "--useFSPD",
+               "-a {}".format(samples[name]['bam']),
+               "-o {}".format(output_dir)
+               ]
+
+    job.fileStore.logToMaster("Salmon Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return output_dir
