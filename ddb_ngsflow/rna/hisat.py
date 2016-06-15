325353d09ed9f8c1716daa7724c49e2fc7fdf571
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-15 11:27:16 -0300

    Adding hisat2 and stringtie wrapper methods

---
+++ ddb_ngsflow/rna/hisat.py
@@ -0,0 +1,116 @@
+"""
+.. module:: hisat
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the HiSat2 RNA alignment program
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+from ddb_ngsflow import pipeline
+
+
+def add_additional_options(command_list, config, flags):
+
+    return command_list
+
+
+def hisat_paired(job, config, name, samples, flags):
+    """Align RNA-Seq data to a reference using HiSat2
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output bam file name.
+    """
+
+    logfile = "{}.hisat.log".format(name)
+    output = "{}.hisat.sorted.bam".format(name)
+    temp = "{}.hisat.sort.temp".format(name)
+
+    hisat_cmd = ["{}".format(config['hisat']['bin']),
+                 "-p {}".format(config['hisat']['num_cores']),
+                 "--dta",
+                 "-x {}".format(config['hisat']['index']),
+                 "-1 {}".format(samples[name]['fastq1']),
+                 "-2 {}".format(samples[name]['fastq2'])
+                 ]
+
+    # hisat_cmd = add_additional_options(hisat_cmd, config, flags)
+
+    view_cmd = ["{}".format(config['samtools']['bin']),
+                "view",
+                "-u",
+                "-"]
+
+    sort_cmd = ["{}".format(config['samtools']['bin']),
+                "sort",
+                "-@",
+                "{}".format(config['hisat']['num_cores']),
+                "-O",
+                "bam",
+                "-o",
+                "{}".format(output),
+                "-T",
+                "{}".format(temp),
+                "-"]
+
+    command = "{} | {} | {}".format(" ".join(hisat_cmd), " ".join(view_cmd), " ".join(sort_cmd))
+
+    job.fileStore.logToMaster("HiSat2 Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return output
+
+
+def hisat_unpaired(job, config, name, samples, flags):
+    """Align RNA-Seq data to a reference using HiSat2
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :returns:  str -- The output bam file name.
+    """
+
+    logfile = "{}.hisat.log".format(name)
+    output = "{}.hisat.sorted.bam".format(name)
+    temp = "{}.hisat.sort.temp".format(name)
+
+    hisat_cmd = ["{}".format(config['hisat']['bin']),
+                 "-p {}".format(config['hisat']['num_cores']),
+                 "--dta",
+                 "-x {}".format(config['hisat']['index']),
+                 "-U {}".format(samples[name]['fastq1'])
+                 ]
+
+    # hisat_cmd = add_additional_options(hisat_cmd, config, flags)
+
+    view_cmd = ["{}".format(config['samtools']['bin']),
+                "view",
+                "-u",
+                "-"]
+
+    sort_cmd = ["{}".format(config['samtools']['bin']),
+                "sort",
+                "-@",
+                "{}".format(config['hisat']['num_cores']),
+                "-O",
+                "bam",
+                "-o",
+                "{}".format(output),
+                "-T",
+                "{}".format(temp),
+                "-"]
+
+    command = "{} | {} | {}".format(" ".join(hisat_cmd), " ".join(view_cmd), " ".join(sort_cmd))
+
+    job.fileStore.logToMaster("HiSat2 Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return output
---
+++ ddb_ngsflow/rna/stringtie.py
@@ -0,0 +1,121 @@
+"""
+.. module:: stringtie
+   :platform: Unix, OSX
+   :synopsis: A module of methods for working with the StringTie transcript assembly and quantification program
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
+
+from ddb_ngsflow import pipeline
+
+
+def add_additional_options(command_list, config, flags):
+    if 'keep_retained' in flags:
+        command_list.append("-i")
+
+    return command_list
+
+
+def stringtie_first(job, config, name, samples, flags):
+    """Perform transcript assembly and quantification with StringTie
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :param flags: Flags for extra parameter settings.
+    :type flags: list.
+    :returns:  str -- The transcript assembly GTF file name.
+    """
+
+    logfile = "{}.stringtie_first.log".format(name)
+    outfile = "{}.stringtie_first.gtf".format(name)
+
+    command = ["{}".format(config['strintie']['bin']),
+               "{}".format(samples[name]['bam']),
+               "-p {}".format(config['stringtie']['num_cores']),
+               "-G {}".format(config['transcript_reference_gff']),
+               "-f 0.05",
+               "-m 100",
+               "-o {}".format(outfile)
+               ]
+
+    command = add_additional_options(command, config, flags)
+
+    job.fileStore.logToMaster("StringTie Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return outfile
+
+
+def stringtie(job, config, name, samples, flags):
+    """Perform transcript assembly and quantification with StringTie
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :param flags: Flags for extra parameter settings.
+    :type flags: list.
+    :returns:  str -- The transcript assembly GTF file name.
+    """
+
+    logfile = "{}.stringtie.log".format(name)
+    outfile = "{}.stringtie.gtf".format(name)
+    abundances_file = "{}.gene_abundances.txt".format(name)
+
+    command = ["{}".format(config['strintie']['bin']),
+               "{}".format(samples[name]['bam']),
+               "-p {}".format(config['stringtie']['num_cores']),
+               "-G {}".format(config['merged_transcripts']),
+               "-A {}".format(abundances_file),
+               "-f 0.05",
+               "-m 100",
+               "-B",
+               "-e",
+               "-o {}".format(outfile)
+               ]
+
+    command = add_additional_options(command, config, flags)
+
+    job.fileStore.logToMaster("StringTie Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return outfile
+
+
+def stringtie_merge(job, config, name, samples, flags):
+    """Perform transcript assembly and quantification with StringTie
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param name: sample name.
+    :type name: str.
+    :param samples: The samples info and config dictionary.
+    :type samples: dict.
+    :param flags: Flags for extra parameter settings.
+    :type flags: list.
+    :returns:  str -- The transcript assembly GTF file name.
+    """
+
+    logfile = "{}.stringtie_merge.log".format(name)
+    outfile = "{}.stringtie.merged.gtf".format(name)
+
+    command = ["{}".format(config['strintie']['bin']),
+               "{}".format(samples[name]['bam']),
+               "--merge",
+               "-p {}".format(config['stringtie']['num_cores']),
+               "-G {}".format(config['merged_transcripts']),
+               "-o {}".format(outfile)
+               ]
+
+    command = add_additional_options(command, config, flags)
+
+    job.fileStore.logToMaster("StringTie Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return outfile
