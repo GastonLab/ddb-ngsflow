5a017ff7421d0666b78e19d012d9607ed9b89ef9
Dan Gaston <daniel.gaston@gmail.com>
2017-05-17 14:47:56 -0300

    Fix joint genotype calling for amplicon data

--- ddb_ngsflow/variation/haplotypecaller.py
+++ ddb_ngsflow/variation/haplotypecaller.py
@@ -126,3 +126,43 @@ def haplotypecaller_amplicon(job, config, name, samples, input_bam):
     pipeline.run_and_log_command(" ".join(command), logfile)
 
     return gvcf
+
+
+def joint_variant_calling_amplicon(job, config, name, samples):
+    """Create a cohort VCF file based on joint calling from gVCF files
+    :param config: The configuration dictionary.
+    :type config: dict.
+    :param sample: sample name.
+    :type sample: str.
+    :param samples: samples configuration dictionary
+    :type samples: dict
+    :param input_bam: The input_bam file name to process.
+    :type input_bam: str.
+    :returns:  str -- The output vcf file name.
+    """
+
+    vcf = "{}.haplotypecaller.vcf".format(name)
+    logfile = "{}.haplotypecaller_gvcf.log".format(name)
+
+    gvcfs = list()
+    for sample in samples:
+        gvcfs.append("--variant {}.haplotypecaller.g.vcf".format(sample))
+
+    gvcf_string = " ".join(gvcfs)
+
+    command = ["{}".format(config['gatk-jointgenotyper']['bin']),
+               "-T",
+               "GenotypeGVCFs",
+               "-R",
+               "{}".format(config['reference']),
+               "{}".format(gvcf_string),
+               "-nt",
+               "{}".format(config['gatk-jointgenotyper']['num_cores']),
+               "-drf DuplicateRead",
+               "-o",
+               "{}".format(vcf)]
+
+    job.fileStore.logToMaster("GenotypeVCFs Command: {}\n".format(command))
+    pipeline.run_and_log_command(" ".join(command), logfile)
+
+    return vcf
